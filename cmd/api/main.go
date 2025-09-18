0ba49c3be803a9e7cc23b8a0e22ec8569dcf0898
Mariusz Dubicki <mariuszdubicki@Mac-mini-Mariusz.local>
2025-09-18 16:25:26 +0200

    Refaktoryzacja: porzadek w folderach start generalny datium-landing-back

---
+++ cmd/api/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+
+
+func main() {
+
+	srv := &http.Server {
+	Addr: "8080",
+	Handler: http.DefaultServeMux,
+
+	}
+
+func (h, *Hello) ServeHTTP()
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Emerzet/datium-back.git
+
+go 1.24.6
