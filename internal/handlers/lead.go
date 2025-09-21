6166b97e5fa95f90aaad02e95ac84b2fa258b29a
Mariusz Dubicki <mariuszdubicki@Cpt-Oiram.local>
2025-09-21 20:28:16 +0200

    Wstep pod back form lead handler

---
+++ internal/handlers/lead.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"net/http"
+)
+
+
+type Lead struct {
+	name string
+	surname string
+	phone string
+	email string
+	
+
+
+}
+
+
+
+
+func LeadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w,"Method Not Allowed", http.StatusMethodNotAllowed)
+		return 
+
+
+	}
+
+}
