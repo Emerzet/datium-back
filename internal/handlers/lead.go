f99b87c50215e504458d81eecfd46b2dd3cc0e46
Mariusz Dubicki <mariuszdubicki@Mac-mini-Mariusz.local>
2025-09-26 01:29:24 +0200

    Dodanie maxbyte dla lead handlera

--- internal/handlers/lead.go
+++ internal/handlers/lead.go
@@ -25,10 +25,12 @@ func LeadHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w,"Method Not Allowed", http.StatusMethodNotAllowed)
 		return 
 	}
+	r.Body = http.MaxBytesReader(w, r.Body, 1<<20)
 
 	err := r.ParseForm()
 	if err != nil {
 		http.Error(w, "bad request", http.StatusBadRequest)
+		return
 	}
 
 }
